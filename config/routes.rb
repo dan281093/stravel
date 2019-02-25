8944ee89a7c055b930d94691728fbd4f8465da41
dan amouyal <danamouyal@hotmail.fr>
2019-02-25 14:15:41 +0200

    routes done

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,13 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/choose'
   root to: 'pages#home'
+  get 'choose', to: 'pages#choose'
+
+  resources :attendees, only: [:index, :show]
+  resources :dashboards, only: [:index]
+  resources :events, only: [:index, :show, :checkout]
+  resources :events do
+    resources :attendees, only: [:index, :show]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
