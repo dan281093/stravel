654c9eca013f846a2e7fb3200444312c65a3e67d
dan amouyal <danamouyal@hotmail.fr>
2019-02-25 18:58:20 +0200

    implement map. miss pin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_25_122048) do
+ActiveRecord::Schema.define(version: 2019_02_25_153707) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2019_02_25_122048) do
     t.bigint "activity_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
     t.index ["activity_id"], name: "index_events_on_activity_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
