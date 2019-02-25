d8442792879d6a28e1e1463e025bb393846d428b
Daniel Tridico <mail@danieltridico.com>
2019-02-25 12:24:52 +0200

    schema is ready

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -1,4 +1,5 @@
 class Attendee < ApplicationRecord
   belongs_to :user
   belongs_to :event
+  # add validation uniqueness with scope
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :events
+  has_many :hosted_events, foreign_key: :host_id, class_name: 'Event'
   has_many :user_interests
+  has_many :interests, through: :user_interests
   has_many :attendees
+  has_many :events, through: :attendees
 end
