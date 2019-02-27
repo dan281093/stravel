88cfa9e4d14a7e63a435d7d0dc6667d121b89d98
nickoma ceaser <nickomaceaser@gmail.com>
2019-02-27 15:51:02 +0200

    added event date into schema and working on past event for user story 4

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,11 @@
 class DashboardsController < ApplicationController
   def index
-    @events = current_user.events
-    @hosting_events = Event.where(host: current_user)
+    @events = current_user.events.map do |event|
+      event.date >= Date.today
+    end
+    # @past_events = current_user.events.where(date > Date.today)
+    @hosting_events = Event.where(host: current_user).map do |event|
+      event.date >= Date.today
+    end
   end
 end
