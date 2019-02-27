329c49cca003374435353a4be80f9abc67a76d3c
dan amouyal <danamouyal@hotmail.fr>
2019-02-27 15:43:41 +0200

    autocomplete address

--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: 'RIA7NA5211', api_key: 'c55a373727a80681b1daab931e9d9e88' }
+AlgoliaSearch.configuration = { application_id: 'RIA7NA5211', api_key: ENV['ALGOLIA_KEY'] }
