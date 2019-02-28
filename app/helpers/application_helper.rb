c8611d4e0930e2e09615fee17e35593bbcddd268
nickoma ceaser <nickomaceaser@gmail.com>
2019-02-28 14:23:43 +0200

    added default user avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def avatar_url(user)
+    if user.photo.url.present?
+      cl_image_path user.photo
+    else
+      image_path 'default_avatar.png'
+    end
+  end
 end
