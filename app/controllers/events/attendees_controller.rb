31cbe49019debbdb6559fb85eaa9c383493a9b9c
Daniel Tridico <mail@danieltridico.com>
2019-03-01 16:08:29 +0200

    backend is working now

--- app/controllers/events/attendees_controller.rb
+++ app/controllers/events/attendees_controller.rb
@@ -1,9 +1,32 @@
 class Events::AttendeesController < ApplicationController
   def index
-    event = Event.find(params[:id])
-    @users =  event.users
+    @event = Event.find(params[:event_id])
+    @attendees = @event.attendees
   end
 
   def show
+    @event = Event.find(params[:event_id])
+    @attendee = Attendee.find(params[:id])
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @attendee = Attendee.new(user_id: current_user.id, event_id: @event.id, status: 'pending')
+    @attendee.save
+    redirect_to dashboards_path
+  end
+
+  def update
+    if params[:query]
+      @attendee = Attendee.find(params[:id])
+      if params[:query] == 'Decline'
+        @attendee.update_attribute(:status, 'declined')
+      else
+        @attendee.update_attribute(:status, 'accepted')
+      end
+      redirect_to event_attendees_path(@attendee.event)
+    else
+      render :show
+    end
   end
 end
